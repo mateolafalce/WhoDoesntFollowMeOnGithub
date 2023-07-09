1a7321dadc5eeb8855aca8e614f1eb8db060ac9e
Mateo Lafalce <mateolafalce3@gmail.com>
2023-07-10 00:59:52 +1100

    username.rs

--- src/io/username.rs
+++ src/io/username.rs
@@ -1,9 +1,12 @@
 pub fn username() -> String {
+    // Prompt the user to enter their Github username
     println!("------------------------------------------");
     println!("Enter your Github username:");
     println!("------------------------------------------");
+    // Read the user's input from the standard input
     let mut user_input = String::new();
     std::io::stdin().read_line(&mut user_input).expect("Failed to read line");
+    // Remove any trailing newline characters and convert the input to a String
     let username = user_input.trim().to_string();
     username
 }
