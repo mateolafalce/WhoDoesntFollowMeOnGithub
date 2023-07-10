18ac89fb62d2e34f80fceaba112a9e68032b68e1
Mateo Lafalce <mateolafalce3@gmail.com>
2023-07-11 00:14:54 +1100

    github_token.rs

--- src/io/github_token.rs
+++ src/io/github_token.rs
@@ -1,9 +1,13 @@
 pub fn github_token() -> String {
+    // Prompt the user to enter their Github token
     println!("------------------------------------------");
     println!("Enter your Github token");
     println!("------------------------------------------");
+    // Create a mutable string to store the user input
     let mut token_input = String::new();
+    // Read the user input from the standard input
     std::io::stdin().read_line(&mut token_input).expect("Failed to read line");
+    // Trim any leading or trailing whitespace and convert the input to a String
     let token: String = token_input.trim().to_string();
     token
 }
